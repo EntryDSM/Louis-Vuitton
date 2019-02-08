abd713424615020744b79a58053bec44d14e9cc7
Lewis <artoria@artoria.us>
2019-02-09 03:16:42 +0900

    ADD: 베이스 디스크립터 클래스 추가

---
+++ server/app/models/descriptor.py
@@ -0,0 +1,12 @@
+class Type:
+    name: str
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
