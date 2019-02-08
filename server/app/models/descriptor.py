2a69a0fac611778caa0907420635deb5fa9bb6ec
Lewis <artoria@artoria.us>
2019-02-09 03:17:18 +0900

    ADD: Integer, String, Enum 디스크립터 구현

--- server/app/models/descriptor.py
+++ server/app/models/descriptor.py
@@ -10,3 +10,37 @@ class Type:
     def __set_name__(self, owner, name):
         self.name = name
 
+
+class Integer(Type):
+    def __init__(self, unsigned=False):
+        self.unsigned = unsigned
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise ValueError("value is not int")
+        if self.unsigned:
+            if value < 0:
+                raise ValueError("negative value for unsigned type")
+        super().__set__(instance, value)
+
+
+class String(Type):
+    def __init__(self, length=0):
+        self.length = 0
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise ValueError("value is not str")
+        if self.length and (len(value) > self.length):
+            raise ValueError("value too long")
+        super().__set__(instance, value)
+
+
+class Enum(Type):
+    def __init__(self, *args):
+        self.keys = args
+
+    def __set__(self, instance, value):
+        if value not in self.keys:
+            raise ValueError(f"Value '{value}' is not in enum key list {self.keys}")
+        super().__set__(instance, value)
