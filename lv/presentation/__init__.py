323fbc01006540dee0744ff910f32031388784c4
JKS <jerion7474@gmail.com>
2019-07-03 22:56:20 +0900

    Add record API interfaces

--- lv/presentation/__init__.py
+++ lv/presentation/__init__.py
@@ -2,10 +2,12 @@ from sanic import Blueprint
 
 from lv.presentation.classification import bp_classification
 from lv.presentation.document import bp_document
+from lv.presentation.record import bp_record
 
 
 api = Blueprint.group(
     bp_classification,
     bp_document,
+    bp_record,
     url_prefix="/applicant/<email>",
 )
